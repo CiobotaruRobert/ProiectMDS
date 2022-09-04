df2f1032dfcf93f3a4f47089bed4463d42d20b48
CiobotaruRobert <94302777+CiobotaruRobert@users.noreply.github.com>
2022-09-04 14:07:05 +0300

    Update RecyclerAdapter.java

--- main/java/com/example/proiect/RecyclerAdapter.java
+++ main/java/com/example/proiect/RecyclerAdapter.java
@@ -58,6 +58,40 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ViewHo
             postImage = itemView.findViewById(R.id.ivPost);
             title = itemView.findViewById(R.id.title);
             message = itemView.findViewById(R.id.message);
+            
+                        itemView.findViewById(R.id.ivBookmark).setOnClickListener(view ->{
+                FirebaseUser currentFirebaseUser = FirebaseAuth.getInstance().getCurrentUser() ;
+                if(currentFirebaseUser==null)
+                    Toast.makeText(itemView.getContext(), "Trebuie sa fiti autentificat pentru a pune un semn de carte",
+                            Toast.LENGTH_SHORT).show();
+                else{
+                    int aux=getAdapterPosition()+1;
+                    if(mydb.check_if_bookmark_unique(aux,MainActivity.id_user_curent)==1)
+                    {
+                        mydb.addBookmark(MainActivity.id_user_curent,aux);
+                    }
+                    else
+                    {
+                        Toast.makeText(itemView.getContext(), "Deja ati pus un semn de carte pentru aceasta postare",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+
+            itemView.findViewById(R.id.ivShare).setOnClickListener(view->{
+                int aux=getPosition()+1;
+                Intent intent = new Intent(Intent.ACTION_SEND);
+                intent.setType("text/plain");
+                String Body = "Titlu_reteta";
+                String Sub = "Detalii";
+                Cursor cursor=mydb.get_title_and_content_from_post(aux);
+                cursor.moveToNext();
+                Body= cursor.getString(0);
+                Sub=cursor.getString(1);
+                intent.putExtra(Intent.EXTRA_TEXT, Body+"\n"+Sub);
+                itemView.getContext().startActivity(Intent.createChooser(intent, "Trimite cu ajutorul:"));
+            });
+            
         }
     }
 }
