9ea0cf74e30e054fe36ecdd0d1386a3738604943
tudor-ghidarcea <81401443+tudor-ghidarcea@users.noreply.github.com>
2022-09-04 23:04:57 +0300

    Update RecyclerAdapter.java

--- main/java/com/example/proiect/RecyclerAdapter.java
+++ main/java/com/example/proiect/RecyclerAdapter.java
@@ -1,13 +1,20 @@
 package com.example.proiect;
 
+import android.content.Intent;
+import android.database.Cursor;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import java.util.ArrayList;
 
@@ -19,6 +26,11 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ViewHo
 
         this.arrayList=arrayList;
 
+    }
+
+    public void setFilteredList(ArrayList<Feed> filteredList){
+        this.arrayList=filteredList;
+        notifyDataSetChanged();
     }
 
     @NonNull
@@ -36,9 +48,11 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ViewHo
         holder.title.setText(feed.getTitle());
         holder.message.setText(feed.getMessage());
         holder.profileImage.setImageResource(feed.getProfileIcon());
-        holder.postImage.setImageResource(feed.getPostImage());
+        holder.postImage.setImageBitmap(feed.getPostImage());
+
 
     }
+
 
     @Override
     public int getItemCount() {
@@ -51,6 +65,8 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ViewHo
         ImageView postImage;
         TextView title;
         TextView message;
+        ImageButton like_button;
+
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
@@ -58,8 +74,65 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ViewHo
             postImage = itemView.findViewById(R.id.ivPost);
             title = itemView.findViewById(R.id.title);
             message = itemView.findViewById(R.id.message);
-            
-                        itemView.findViewById(R.id.ivBookmark).setOnClickListener(view ->{
+            like_button=itemView.findViewById(R.id.ivLike);
+
+            DatabaseHelper mydb=new DatabaseHelper(itemView.getContext());
+
+            TextView like_counter=itemView.findViewById(R.id.like_counter);
+            like_counter.setText(String.valueOf(mydb.get_like_counter(getAdapterPosition()+1)));
+
+            itemView.findViewById(R.id.ivLike).setOnClickListener(view ->{
+                FirebaseUser currentFirebaseUser = FirebaseAuth.getInstance().getCurrentUser() ;
+                if(currentFirebaseUser==null)
+                    Toast.makeText(itemView.getContext(), "Trebuie sa fiti autentificat pentru a aprecia postarea",
+                            Toast.LENGTH_SHORT).show();
+                else{
+                    int aux=getAdapterPosition()+1;
+                    if(mydb.check_if_like_unique(aux,MainActivity.id_user_curent)==1) {
+                        mydb.add_like_to_post(aux,MainActivity.id_user_curent);
+                        Toast.makeText(itemView.getContext(), "Ati apreciat postarea", Toast.LENGTH_SHORT).show();
+                        like_counter.setText(String.valueOf(mydb.get_like_counter(getAdapterPosition()+1)));
+
+                    }
+                    else
+                    {
+                        mydb.undo_like_to_post(aux,MainActivity.id_user_curent);
+                        Toast.makeText(itemView.getContext(), "Acum nu mai apreciati postarea", Toast.LENGTH_SHORT).show();
+                        like_counter.setText(String.valueOf(mydb.get_like_counter(getAdapterPosition()+1)));
+                    }
+                    }
+            });
+            itemView.findViewById(R.id.ivComment).setOnClickListener(view ->{
+                FirebaseUser currentFirebaseUser = FirebaseAuth.getInstance().getCurrentUser() ;
+                if(currentFirebaseUser==null)
+                    Toast.makeText(itemView.getContext(), "Trebuie sa fiti autentificat pentru a putea comenta",
+                            Toast.LENGTH_LONG).show();
+                else
+                {
+//                    Intent sectiune_comentarii = new Intent(itemView.getContext(),CommentActivity.class);
+//                    itemView.getContext().startActivity(sectiune_comentarii);
+
+                }
+
+            });
+//        if(MyPosts.active) {
+//            ImageView btn=(ImageView)itemView.findViewById(R.id.menu);
+//            btn.setImageResource(R.drawable.ic_baseline_delete_24);
+//            itemView.findViewById(R.id.menu).setOnClickListener(view -> {
+//                FirebaseUser currentFirebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+//                int aux=getAdapterPosition()+1;
+//                mydb.delete_post(aux,MainActivity.id_user_curent);
+//                notifyDataSetChanged();
+//            });
+//        }
+
+            itemView.findViewById(R.id.ivPost).setOnClickListener(view->{
+                like_counter.setText(String.valueOf(mydb.get_like_counter(getAdapterPosition()+1)));
+                itemView.findViewById(R.id.ivPost).setSoundEffectsEnabled(false);
+            });
+
+
+            itemView.findViewById(R.id.ivBookmark).setOnClickListener(view ->{
                 FirebaseUser currentFirebaseUser = FirebaseAuth.getInstance().getCurrentUser() ;
                 if(currentFirebaseUser==null)
                     Toast.makeText(itemView.getContext(), "Trebuie sa fiti autentificat pentru a pune un semn de carte",
@@ -91,7 +164,6 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ViewHo
                 intent.putExtra(Intent.EXTRA_TEXT, Body+"\n"+Sub);
                 itemView.getContext().startActivity(Intent.createChooser(intent, "Trimite cu ajutorul:"));
             });
-            
         }
     }
 }
